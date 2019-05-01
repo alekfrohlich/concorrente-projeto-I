425a8f42457fd225bcfbb13ec02c07fb7af9d2b4
alek.frohlich <alek.frohlich@gmail.com>
2019-05-01 18:11:50 -0300

    ready for branch

--- src/pizzeria.c
+++ src/pizzeria.c
@@ -13,9 +13,14 @@ pthread_mutex_t pegando_mesas, liberando_mesas;
 int open;
 int num_mesas;
 
+queue_t* smart_deck;
+
 void pizzeria_init(int tam_forno, int n_pizzaiolos, int n_mesas,
                    int n_garcons, int tam_deck, int n_grupos) {
     // inicializando mutexes, semaforos e estruturas de dados...
+    smart_deck = malloc(sizeof(queue_t));
+	queue_init(smart_deck, tam_deck);
+
     sem_init(&garcons_livres, 0, n_garcons);
     sem_init(&mesas_livres, 0 , n_mesas);
     sem_init(&forno_livre, 0, tam_forno);
@@ -26,12 +31,15 @@ void pizzeria_init(int tam_forno, int n_pizzaiolos, int n_mesas,
 }
 
 void pizzeria_close() {
-    // pensando bem acho que nao precisa esperar, so nao pode sentar pessoas depois que fecha e pra isso ja tem o open = 0
+    // precisa esperar alguem terminar de fazer alguma coisa? vide email.
     open = 0;
 }
 
 void pizzeria_destroy() {
     // destroi mutexes, semaforos e estruturas de dados...
+    queue_destroy(smart_deck);
+    free(smart_deck);
+
     sem_destory(&mesas_livres);
     sem_destroy(&garcons_livres);
     sem_destroy(&forno_livre);
@@ -40,13 +48,13 @@ void pizzeria_destroy() {
 }
 
 void pizza_assada(pizza_t* pizza) {
-    // Pode fazer isso com um semaforo?
+    // libera pizzaiolo_retirar_pizza_forno() e nao faz mais nada? vide email.
     sem_post(&pizza.esperando_assar);
 }
 
 int pegar_mesas(int tam_grupo) {
-    pthread_mutex_lock(&pegando_mesas);
     int mesas = ceil(tam_grupo/4);
+    pthread_mutex_lock(&pegando_mesas);
     for (int i = 0; i < mesas; i++)
         sem_wait(&mesas_livres);
     if (mesas <= num_mesas && open) {
@@ -59,14 +67,15 @@ int pegar_mesas(int tam_grupo) {
 }
 
 void garcom_tchau(int tam_grupo) {
-    pthread_mutex_lock(&liberando_mesas);
     int mesas = ceil(tam_grupo/4);
+    pthread_mutex_lock(&liberando_mesas);
     for (int i = 0; i < mesas; i++)
         sem_post(&mesas_livres);
     pthread_mutex_unlock(&liberando_mesas);
 }
 
 void garcom_chamar() {
+`   // essa funcao representa soh um delay? vide email.
     sem_wait(&garcons_livres);
     sem_post(&gracons_livres);
 }
@@ -77,11 +86,14 @@ void fazer_pedido(pedido_t* pedido) {
     // falta controlar a ordenacao dos pedidos:
         sem_wait(&pizzaiolos_livres); 
             pizza_t * pizza = pedido_montar_pizza(pedido);
+            pthread_mutex_init(&pizza->pegador_de_pizza, NULL);
+            sem_init(&pizza->esperando_assar, 0, 0);
             sem_wait(&forno_livre);
             pthread_mutex_lock(&pa_de_pizza);
                 pizzaiolo_colocar_pizza_forno(pedido);
             pthread_mutex_unlock(&pa_de_pizza);
-            sem_wait(&pizza.esperando_assar);
+            sem_wait(&pizza->esperando_assar);
+            sem_destroy(&pizza->esperando_assar);
             pthread_mutex_lock(&pa_de_pizza);
                 pizzaiolo_retirar_pizza_forno(pedido);
             pthread_mutex_unlock(&pa_de_pizza);
@@ -92,16 +104,15 @@ void fazer_pedido(pedido_t* pedido) {
 }
 
 int pizza_pegar_fatia(pizza_t* pizza) {
-    // ATENCAO: a sintaxe pizza.pegador_de_pizza pode estar errada (favor verificar)
-    // e pode dar memory leak o esquema de criar um mutex pra cada pizza (mas pede na descricao)
-    // entao ficar bem atento que esse mutex esta sem seu pthread_mutex_destroy()!!!
-    pthread_mutex_init(&pizza->pegador);
+    // ATENCAO: como inicializar o pegador e o esperando_assar sem quebrar horrendamente
+    // quando varias pessoas tentarem comer pizza dps que nao tem mais pizza? Nao perguntamos ainda!!!
     pthread_mutex_lock(&pizza->pegador);
-    if (pizza.fatias > 0) {
-        pizza.fatias -= 1;
+    if (pizza->fatias > 0) {
+        pizza->fatias -= 1;
         pthread_mutex_unlock(&pegador);
         return 0;
     }
     pthread_mutex_unlock(&pegador);
+    pthread_mutex_destroy(&pizza->pegador);
     return -1;
 }
