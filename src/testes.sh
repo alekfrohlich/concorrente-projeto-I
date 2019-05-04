54d5912e1abcf492e16a6b2c01236b6788519254
alek.frohlich <alek.frohlich@gmail.com>
2019-05-04 19:44:53 -0300

    test script

---
+++ src/testes.sh
@@ -0,0 +1,11 @@
+make
+#echo "testando cenario Mini forno..."
+#./program 2 10 40 40 40 40 5
+echo "testando cenario Greve de Pizzaiolos..." 
+./program 4 2 40 40 40 40 1
+echo "testando cenario Inflacao moveleira..."
+./program 10 10 10 10 40 40 1
+echo "testando cenario Greve de garcons..."
+./program 10 10 40 2 40 40 1
+echo "testando cenario Escassez de fichas ..."
+./program 10 10 40 40 3 40 1
